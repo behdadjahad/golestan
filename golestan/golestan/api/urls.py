e17a24553d512e5c11d901595901851c51767aa2
behdadjahad <behdadja@gmail.com>
2023-11-21 10:32:04 +0330

    changing some files

--- golestan/golestan/api/urls.py
+++ golestan/golestan/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', include(('golestan.account.urls', 'student')))
+    path('admin/', include(('golestan.account.urls', 'admin entities'))),
+    # path("assistant", include('golestan.faculty.urls', 'assistant entities'))
 ]
